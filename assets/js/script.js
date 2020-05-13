fbcb60c060790dcb400785679e98e3ea388294bb
Tanner Griffin <tannermgriffin@gmail.com>
2020-05-12 22:01:45 -0500

    created a single row (w/ hour div, text area, and button), appended to parent elements. additional styling to match example

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,21 @@
 var today = moment().format('ddd, MMMM Do YYYY');
 $('#currentDay').text(today);
+
+// create row in container
+var row = $('<div>');
+row.attr('class', 'row');
+row.attr('data-timeslot', '0');
+$('.container').append(row);
+// create hour div in row
+var hour = $('<div>');
+hour.attr('class', 'hour');
+hour.text('9AM');
+$('.row').append(hour);
+// create text area in row
+var textArea = $('<textarea>');
+$('.row').append(textArea);
+// create button in row
+var saveBtn = $('<button>');
+saveBtn.attr('class', 'saveBtn');
+saveBtn.html('<i class="fas fa-save"></i>');
+$('.row').append(saveBtn);
